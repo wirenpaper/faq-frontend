070726577364fbfeda7d6d000128095d8682fd1e
wnp <chumchumchira@gmail.com>
2024-06-15 14:09:25 +0500

    h2 specifications completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ export default function Home() {
     <div>
       <h1 className="text-center p-5">Project 2: FAQ/Accordian</h1>
       <div>
-      <h3 className="bg-blue-800 flex justify-center">Frequently asked questions</h3>
+        <h2 className="bg-blue-800 text-white flex justify-center">
+          Frequently Asked Questions
+        </h2>
       </div>
     </div>
   );
